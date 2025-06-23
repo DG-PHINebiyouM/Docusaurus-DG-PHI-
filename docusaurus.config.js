bc85e873d4b942b2002a5e6f426b0548a25ec6d0
monty-nebiyou-dg <Mathias.Nebiyou@bwdesigngroup.com>
2025-06-23 08:39:56 -0400

    testing change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'BW DG-PHI Documentation',
-  tagline: 'Directors: Steve Chapman, Tsuyoshi Kuroki',
+  tagline: 'Director: Tsuyoshi Kuroki',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
